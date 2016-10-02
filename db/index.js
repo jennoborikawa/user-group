be23dc7b76a66f3449eb724703c70e9925b3f02e
tomtrasmontero <tomtrasmontero@gmail.com>
2016-10-02 11:48:21 -0400

    models completed

--- db/index.js
+++ db/index.js
@@ -1,18 +1,22 @@
 var db = require('./_db');
 
 var User = require('./user');
+var Group = require('./group');
 
 module.exports = {
   syncAndSeed: function(){
     return db.sync({ force: true })
       .then(function(){
         return Promise.all([
-            User.create({ username: 'moe' }),
-            User.create({ username: 'larry' }),
+            Group.create({name: 'Admin'}),
+            Group.create({name: 'HR'}),
+            User.create({ username: 'moe', password: 'foo', groupId: 1}),
+            User.create({ username: 'larry', password: 'bar', groupId: 1 }),
         ]);
       })
   },
   models: {
-    User: User
+    User: User,
+    Group: Group
   }
 };
